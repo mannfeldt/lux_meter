fb0900377b013b21679e15f4e7f8ba33d0f5a694
emilmannfeldt <emil.mannfeldt@gmail.com>
2020-05-10 16:21:27 +0200

    lux done

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,313 @@
+import 'dart:async';
+
+import 'package:device_preview/device_preview.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:light/light.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    DevicePreview(
+      enabled: false,
+      builder: (BuildContext context) => MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LuxMeter(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class LuxMeter extends StatefulWidget {
+  LuxMeter({Key key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _LuxMeterState createState() => _LuxMeterState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _LuxMeterState extends State<LuxMeter> {
+  List<int> lux = [0];
+  Light _light = new Light();
+  StreamSubscription lightStream;
+  bool showAvg = false;
+  double maxX = 30.0;
+  double maxY = 10.0;
+  List<Color> gradientColors = [
+    Colors.blue.shade800,
+    Colors.red.shade200,
+  ];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  double get maxLux =>
+      lux.fold<double>(0, (max, lux) => lux > max ? lux.toDouble() : max);
+  double get minLux =>
+      lux.fold<double>(100000, (min, lux) => lux < min ? lux.toDouble() : min);
+
+  @override
+  void initState() {
+    startLightStream();
+    super.initState();
+  }
+
+  @override
+  dispose() {
+    lightStream.cancel();
+    super.dispose();
+  }
+
+  void onLightData(int luxValue) async {
+    //kanske bara lägga till om det skiljer mer än x procent eller enhter från föregående?
+    if (lux.isEmpty || lux.last != luxValue) {
+      setState(() {
+        lux.add(luxValue);
+        if (lux.length > maxX + 1) {
+          lux.removeAt(0);
+        }
+      });
+    }
+  }
+
+  void startLightStream() {
+    _light = new Light();
+    try {
+      lightStream = _light.lightSensorStream.listen(onLightData);
+    } on LightException catch (exception) {
+      print(exception);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color(0xff232d37),
+        body: Center(
+          child: Stack(
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: MediaQuery.of(context).size.width /
+                    MediaQuery.of(context).size.height,
+                child: Container(
+                  decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(18),
+                      ),
+                      color: Color(0xff232d37)),
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        right: 30.0, left: 0.0, top: 30, bottom: 10),
+                    child: LineChart(
+                      mainData(),
+                      swapAnimationDuration: Duration(milliseconds: 800),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned.fill(
+                top: 2,
+                child: Container(
+                  child: Text(
+                    "${lux[lux.length - 1]} lux",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.white, fontSize: 18, letterSpacing: 2),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  List<FlSpot> getLuxSpots() {
+    List<FlSpot> luxSpots = [];
+    double max = maxLux;
+    double min = minLux;
+
+    for (int i = 0; i < lux.length; i++) {
+      double luxValue = lux[i].toDouble();
+      double factorValue = (luxValue - min) / (max - min) * maxY;
+      luxSpots.add(FlSpot(i.toDouble(), factorValue));
+    }
+
+    return luxSpots;
+  }
+
+  int roundNicely(double v) {
+    double min = minLux;
+    double max = maxLux;
+
+    double diffRatio = (max - min) / max;
+
+    double value = ((max - min) / maxY) * v + min;
+
+    if (diffRatio > 0.5) {
+      return roundSuper(value);
+    }
+    if (diffRatio > 0.2) {
+      return roundHard(value);
+    }
+    if (diffRatio > 0.02) {
+      return roundMedium(value);
+    }
+    return roundSoft(value);
+  }
+
+  int roundSoft(double value) {
+    if (value < 10000) {
+      return value.round();
+    }
+    if (value < 100000) {
+      return (value / 10).round() * 10;
+    }
+    return (value / 100).round() * 100;
+  }
+
+  int roundMedium(double value) {
+    if (value < 1000) {
+      return value.round();
+    }
+    if (value < 10000) {
+      return (value / 10).round() * 10;
+    }
+    if (value < 100000) {
+      return (value / 100).round() * 100;
+    }
+    return (value / 1000).round() * 1000;
+  }
+
+  int roundHard(double value) {
+    if (value < 100) {
+      return value.round();
+    }
+    if (value < 1000) {
+      return (value / 10).round() * 10;
+    }
+    if (value < 10000) {
+      return (value / 100).round() * 100;
+    }
+    if (value < 100000) {
+      return (value / 1000).round() * 1000;
+    }
+    return (value / 10000).round() * 10000;
+  }
+
+  int roundSuper(double value) {
+    if (value < 10) {
+      return value.round();
+    }
+    if (value < 100) {
+      return (value / 10).round() * 10;
+    }
+    if (value < 1000) {
+      return (value / 100).round() * 100;
+    }
+    if (value < 10000) {
+      return (value / 1000).round() * 1000;
+    }
+    if (value < 100000) {
+      return (value / 10000).round() * 10000;
+    }
+    return (value / 100000).round() * 100000;
+  }
+
+  LineChartData mainData() {
+    return LineChartData(
+      gridData: FlGridData(
+        show: true,
+        drawVerticalLine: false,
+        getDrawingHorizontalLine: (value) {
+          return FlLine(
+            color: const Color(0xff37434d),
+            strokeWidth: 1,
+          );
+        },
+        getDrawingVerticalLine: (value) {
+          return FlLine(
+            color: const Color(0xff37434d),
+            strokeWidth: 1,
+          );
+        },
+      ),
+      titlesData: FlTitlesData(
+        show: true,
+        bottomTitles: SideTitles(
+          showTitles: true,
+          reservedSize: 22,
+          textStyle: const TextStyle(
+              color: Color(0xff68737d),
+              fontWeight: FontWeight.bold,
+              fontSize: 16),
+          getTitles: (value) {
+            return '';
+          },
+          margin: 8,
+        ),
+        leftTitles: SideTitles(
+          showTitles: true,
+          textStyle: const TextStyle(
+            color: Color(0xff67727d),
+            fontWeight: FontWeight.bold,
+            fontSize: 12,
+          ),
+          getTitles: (value) {
+            switch (value.toInt()) {
+              case 0:
+                return "${roundNicely(0)}";
+              case 3:
+                return "${roundNicely(3)}";
+
+              case 6:
+                return "${roundNicely(6)}";
+
+              case 9:
+                return "${roundNicely(9)}";
+            }
+            return '';
+          },
+          reservedSize: maxLux >= 10000 ? 38 : 28,
+          margin: 5,
+        ),
+      ),
+      borderData: FlBorderData(
+          show: true,
+          border: Border.all(color: const Color(0xff37434d), width: 1)),
+      minX: 0,
+      maxX: maxX,
+      minY: 0,
+      maxY: maxY,
+      lineBarsData: [
+        LineChartBarData(
+          spots: getLuxSpots(),
+          gradientFrom: Offset(1, 1),
+          gradientTo: Offset(1, 0),
+          //color stops
+          //Gradient from och to är vad jag ska kolla på?
+          isCurved: true,
+          colors: gradientColors,
+          barWidth: 2,
+          isStrokeCapRound: true,
+          dotData: FlDotData(show: false),
+          belowBarData: BarAreaData(
+            gradientFrom: Offset(1, 1),
+            gradientTo: Offset(1, 0),
+
+            show: true, //Frägen ska vara beroende på max lux ttyp?
+            colors:
+                gradientColors.map((color) => color.withOpacity(0.3)).toList(),
+          ),
+        ),
+      ],
     );
   }
 }
